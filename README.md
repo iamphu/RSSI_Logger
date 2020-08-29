f96d6b2e384a33e3649c76bb7d09730459d4f782
Phu Nguyen <t.l.p.nguyen@student.tudelft.nl>
2020-08-29 08:23:37 +0200

    Uploaded the code, .apk and updated the README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# RSSI_Logger
+# RSSI Logger
+RSSI Logger is a simple app that can read out RSSI values over time and store them on your Android smartphone. The app is not on the Google Play Store and can only be downloaded on this Github page or by building the code yourself. You can build the code by using [Android Studio](https://developer.android.com/studio).
+
+## The App
+### Installation
+The app can be found under the name `RSSI_Logger.apk`. After downloading the apk file on your smartphone, you can start with installing the app. If this is your first time downloading an apk outside of the Google Play Store, you will get a warning that the install is blocked. If this is the case, then you have to go to **Settings** -> **Security**, and allow the installation of apps from unknown sources. After downloading the app you can turn this off again. As the app is not on the Google Play Store, the Play Protect may kick in as it is does not recognize this app. Once you approve the installation the app should be ready to use.
+
+### Permissions
+RSSI Logger only requires two permissions, namely:
+* `android.permission.ACCESS_WIFI_STATE` - Allows the app to have access to information about Wi-Fi networks
+* `android.permission.WRITE_EXTERNAL_STORAGE` - Allows the app to write the data to the external storage for easier file transfers
+
+When the app is first opened, the app requests permission to write to the external storage. Approving it will allow the app to function normally, refusing the permission means that the app is unable to store data and it will close itself to avoid any conflicts later on.
+
+## First Use
+The UI is straightforward; start measuring, saving the data and clear data. Clicking the start measuring button will start a scheduler to read out the RSSI value every **0.5 seconds** (this can be changed in the code itself). The app will continue measuring until the user stops the measurement. The user can give a description of the measurement to the filename, after which it will save in the default external directory. A possible path to the saved data is: `/Android/data/com.adhocnetworks.rssi_logger/files/`. Another way to find the path is to search for the filename which will shortly show up on the bottom of the screen right after saving the file. After everything has been saved, the clear button can be used to clear all old data and start a new measurement.
+
+![Home screen of RSSI Logger](https://i.imgur.com/ksw3j2z.jpg)
+
+> **This app is made for students following the course ET4388 Ad-hoc Networks 2020-2021 at the Delft University of Technology**
---
+++ app/src/main/java/com/adhocnetworks/rssi_logger/MainActivity.java
@@ -0,0 +1,199 @@
+package com.adhocnetworks.rssi_logger;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private int numSamples = 0; // Keeps track of the amount of samples
+    private String saveDescription = ""; // Custom description by the user
+    private boolean measureIsOn = false; // Measurement has started or not
+    private ArrayList<Integer> RSSI_list = new ArrayList<Integer>(); // Keeps track of the RSSI values
+
+    Handler handler = new Handler();    // Handler and runnable to create recurring measuring
+    Runnable myRunnable;
+    int delay = 500; //milliseconds
+
+    // WiFi variables
+    private WifiManager wifiManager;
+    private WifiInfo wifiInfo;
+
+    // Component variables
+    private TextView samples, values;
+    private Button buttonStart, buttonClear, buttonSave;
+
+    // Permission variables
+    public static final int WRITE_PERMISSION = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create textviews
+        samples = (TextView) findViewById(R.id.textView_Samples);
+        values = (TextView) findViewById(R.id.textView_RSSI);
+
+        // Create buttons
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonClear = (Button) findViewById(R.id.buttonClear);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
+        // Set WiFi manager
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        // Create click listeners
+        buttonStart.setOnClickListener(this);
+        buttonClear.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
+
+        // Create recurring measurement + update
+        myRunnable = new Runnable() {
+            public void run() {
+                Log.d("Runnable", "Runnable created");
+                wifiInfo = wifiManager.getConnectionInfo();
+                values.setText(String.valueOf(wifiInfo.getRssi()));
+                RSSI_list.add(wifiInfo.getRssi());
+                numSamples++;
+                samples.setText("Amount of samples: " + numSamples);
+                handler.postDelayed(myRunnable, delay);
+            }
+        };
+
+        // Check if permission has been granted already, if not requests for permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_PERMISSION);
+        }
+    }
+
+    // Requests for permission to write to external storage (to store the RSSI values)
+    // If the user does not give permission, the app can not function and will exit
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case WRITE_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this,  Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Log.d("Permission", "Permission has been granted");
+                    }
+                } else {
+                    finish();
+                    System.exit(0);
+                }
+                return;
+            }
+        }
+    }
+
+    // A writer that will save the data to a .txt file that can be opened on a computer
+    private void writeToFile(String data, String filename) {
+        try {
+            File file = new File(this.getExternalFilesDir(null), filename + ".txt");
+            FileOutputStream fileOutput = new FileOutputStream(file);
+            OutputStreamWriter os = new OutputStreamWriter(fileOutput);
+            os.write(data);
+            os.flush();
+            os.close();
+            Toast.makeText(this, "Data is saved as: " + filename + ".txt", Toast.LENGTH_LONG).show();
+            Log.d("Data", data);
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    // onClick events
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonStart: { // Turn measuring on or off
+                if (measureIsOn == false) {
+                    Log.d("Runnable", "Runnable started");
+                    buttonStart.setText("Stop Measuring");
+                    buttonSave.setEnabled(false);
+                    buttonClear.setEnabled(false);
+                    handler.postDelayed(myRunnable, delay);
+                    measureIsOn = true;
+                } else {
+                    Log.d("Runnable", "Runnable stopped");
+                    buttonStart.setText("Start Measuring");
+                    handler.removeCallbacks(myRunnable);
+                    buttonSave.setEnabled(true);
+                    buttonClear.setEnabled(true);
+                    measureIsOn  = false;
+                }
+                break;
+            }
+            case R.id.buttonClear: { // Clear data
+                Log.d("Button", "Data cleared");
+                RSSI_list.clear();
+                numSamples = 0;
+                samples.setText("Amount of samples: " + numSamples);
+                values.setText(String.valueOf(0));
+                break;
+            }
+            case R.id.buttonSave: { // Save data
+                Log.d("Runnable", "Save button clicked");
+
+                // Creates a prompt for the user to write the description
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Input filename description");
+
+                // Set up the input
+                final EditText input = new EditText(this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        saveDescription = input.getText().toString();
+                        DateFormat df = new SimpleDateFormat("yyMMdd-HHmm");
+                        writeToFile(RSSI_list.toString(), df.format(new Date()) + "_" + saveDescription);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
